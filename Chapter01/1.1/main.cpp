67f49bee61bb2e9622bd0bff145ba274cea75937
Bai <bst1367@outlook.com>
2023-02-18 23:55:47 +0800

    2023/02/18 更新readme 1.1 main.cpp

---
+++ Chapter01/1.1/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @brief C++ Primer 1.1
+ * @version 0.1
+ * @date 2023-02-17
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+int main()
+{
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 # C++ Primer
+______
+## Chapter 1 Getting Started
+"The way to learn a new programming language is to write programs."
+### 1.1 Writing a Simple C++ Program
 
